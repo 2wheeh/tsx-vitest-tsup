548ae5b3b4cde40782e25603142074ba282776b4
wnhlee <2wheeh@gmail.com>
2024-07-22 16:09:29 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { add } from '@/lib/add';
+
+console.log('Hello World!');
+
+console.log(add(1, 2)); // 3
---
+++ src/lib/notUsed.ts
@@ -0,0 +1,5 @@
+export function notUsed() {
+  console.log('not used');
+}
+
+// this should be tree-shaken by tsup
